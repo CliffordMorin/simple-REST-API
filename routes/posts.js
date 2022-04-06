8a7e42ad8185fddfa03385a61f8d7edd64fdcda1
Clifford Morin <cemorin21@gmail.com>
2022-04-06 03:34:00 -0400

    udate a post and get a single post

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("We are on posts");
+//gets all posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
-router.get("/:id", (req, res) => {
-  res.send("We are on a post");
+//gets a single post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//submits a post
+router.post("/", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.send({ message: err });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 require("dotenv").config();
+
+//makes it so that the app can send and receive json
+app.use(bodyParser.json());
 
 //Import ROUTES
 const postsRoute = require("./routes/posts");
